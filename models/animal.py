3b7e6e356eb1ad507dae82c8b7b9f23c56f00f21
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-07 15:42:11 +0000

    Added keeper attribute to Animal

--- models/animal.py
+++ models/animal.py
@@ -1,5 +1,6 @@
 class Animal:
-    def __init__(self, name, type, id = None):
+    def __init__(self, name, type, keeper, id = None):
         self.name = name
         self.type = type
+        self.keeper = keeper
         self.id = id
