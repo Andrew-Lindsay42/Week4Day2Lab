a48e421bb50015eb7f2b5f0bd3220a2a5084e026
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-07 15:01:28 +0000

    Added ability to add a member of staff

---
+++ console.py
@@ -0,0 +1,9 @@
+from models.staff import Staff
+
+import repositories.staff_repository as staff_repo
+
+# staff_repo.delete_all()
+
+
+staff1 = Staff('Andrew', '02/05/2021', 'Keepers', 5)
+staff_repo.save(staff1)
---
+++ repositories/staff_repository.py
@@ -0,0 +1,9 @@
+from db.run_sql import run_sql
+from models.staff import Staff
+
+def save(staff):
+    sql = 'INSERT INTO staff (name, start_date, department, performance) VALUES (%s, %s, %s, %s) RETURNING *'
+    values = [staff.name, staff.start_date, staff.department, staff.performance]
+    result = run_sql(sql, values)
+    staff.id = result[0]['id']
+    return staff
