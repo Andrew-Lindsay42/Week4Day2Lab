bf108b697b0f4d5566765b65b348f10223fe7e5c
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-07 15:02:52 +0000

    Added delete_all method to clear out database

--- console.py
+++ console.py
@@ -2,7 +2,7 @@ from models.staff import Staff
 
 import repositories.staff_repository as staff_repo
 
-# staff_repo.delete_all()
+staff_repo.delete_all()
 
 
 staff1 = Staff('Andrew', '02/05/2021', 'Keepers', 5)
--- repositories/staff_repository.py
+++ repositories/staff_repository.py
@@ -1,6 +1,10 @@
 from db.run_sql import run_sql
 from models.staff import Staff
 
+def delete_all():
+    sql = "DELETE FROM staff"
+    run_sql(sql)
+
 def save(staff):
     sql = 'INSERT INTO staff (name, start_date, department, performance) VALUES (%s, %s, %s, %s) RETURNING *'
     values = [staff.name, staff.start_date, staff.department, staff.performance]
