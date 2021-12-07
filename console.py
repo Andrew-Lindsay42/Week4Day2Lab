e3000ff255de4d1c6e3c844f9cbd3352746eb5ba
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-07 15:36:30 +0000

    Added sql for animals table

--- console.py
+++ console.py
@@ -10,17 +10,13 @@ staff_repo.save(staff1)
 staff2 = Staff('Joe Exotic', '20/03/2020', 'Keepers', 1)
 staff_repo.save(staff2)
 
-res = staff_repo.list_all()
-for staff in res:
-    print(staff.__dict__)
+# print(staff_repo.find_staff(staff2.id).__dict__)
 
-print(staff_repo.find_staff(staff2.id).__dict__)
+# staff_repo.remove_staff(staff2.id)
 
-staff_repo.remove_staff(staff2.id)
-
-staff1.name = 'Andrew Lindsay'
-staff1.performance = 6
-staff_repo.update(staff1)
+# staff1.name = 'Andrew Lindsay'
+# staff1.performance = 6
+# staff_repo.update(staff1)
 
 res = staff_repo.list_all()
 for staff in res:
